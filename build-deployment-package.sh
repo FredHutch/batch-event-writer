258807f2de5af0f68feb26224b1c8c0b20341f07
Dan Tenenbaum <dtenenba@fhcrc.org>
2017-11-17 09:57:25 -0800

    add update-function-code step

--- build-deployment-package.sh
+++ build-deployment-package.sh
@@ -4,14 +4,20 @@ set -e
 
 # workon batch-event-writer
 
+echo adding site-packages...
 pkgdir=$VIRTUAL_ENV/lib/python3.6/site-packages
 
 zip_home=$(pwd)
 
 cd $pkgdir
 
-zip -r9 $zip_home/batch-event-writer.zip *
+zip -qr9 $zip_home/batch-event-writer.zip *
 
 cd $zip_home
 
+echo adding lambda function...
 zip -g batch-event-writer.zip lambda_function.py
+
+echo updating function code...
+aws lambda update-function-code --function-name batch_event_writer \
+  --zip-file fileb://batch-event-writer.zip
