308cdc79fb91bf2e24cb44283715bc4185f3b0ba
Dan Tenenbaum <dtenenba@fhcrc.org>
2017-11-17 12:19:31 -0800

    Insert job-group-related values at top level of record

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,7 @@
 
 import os
 import json
+import datetime
 
 from pymongo import MongoClient
 
@@ -14,8 +15,8 @@ def handler(event, context):
     print("event type is {}".format(event.__class__.__name__))
     print("context is {}".format(context))
     client = MongoClient(os.getenv("MONGO_URL"))
-    db0 = client.testdb
-    coll = db0['mycoll']
+    db0 = client.batch_events
+    coll = db0['events']
     print("event keys are: {}".format(event.keys()))
     print("type of event['body']: {}".format(event['body'].__class__.__name__))
     print("event body:\n{}".format(event['body']))
@@ -26,6 +27,20 @@ def handler(event, context):
     if isinstance(obj['Message'], str):
         message = json.loads(obj['Message'])
     else:
-        message = obj['message']
-    coll.insert_one(message)
+        message = obj['Message']
+    detail = message['detail']
+    detail['timestamp'] = datetime.datetime.now()
+    # copy JOB_GROUP_* environment vars (if any) to top level
+    if 'container' in detail and 'environment' in detail['container']:
+        env = detail['container']['environment']
+        job_group_vars = [{x['name']: x['value']}  \
+            for x in env if x['name'].startswith('JOB_GROUP_')]
+        for item in job_group_vars:
+            key = list(item.keys())[0]
+            detail[key] = item[key]
+
+    result = coll.insert_one(detail)
+    print("Result of insert_one: {}".format(result))
+    print("Acknowledged? {}".format(result.acknowledged))
+    print("ID of inserted object: {}".format(str(result.inserted_id)))
     print("Done")
