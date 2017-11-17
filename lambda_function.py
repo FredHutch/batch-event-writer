cc6161d91f3801892d78c573d15943c0bc468fa6
Dan Tenenbaum <dtenenba@fhcrc.org>
2017-11-16 16:06:17 -0800

    writes correct portion of message that comes in from SNS

--- lambda_function.py
+++ lambda_function.py
@@ -2,15 +2,30 @@
 
 "Lambda function to write a Batch state transition event to MongoDb"
 
-from pymongo import MongoClient
 import os
+import json
+
+from pymongo import MongoClient
+
 
 def handler(event, context):
     "Lambda event handler"
     print("event is {}".format(event))
+    print("event type is {}".format(event.__class__.__name__))
     print("context is {}".format(context))
     client = MongoClient(os.getenv("MONGO_URL"))
     db0 = client.testdb
     coll = db0['mycoll']
-    coll.insert_one({"lambda_says": "hello"})
+    print("event keys are: {}".format(event.keys()))
+    print("type of event['body']: {}".format(event['body'].__class__.__name__))
+    print("event body:\n{}".format(event['body']))
+    if isinstance(event['body'], str):
+        obj = json.loads(event['body'])
+    else:
+        obj = event['body']
+    if isinstance(obj['Message'], str):
+        message = json.loads(obj['Message'])
+    else:
+        message = obj['message']
+    coll.insert_one(message)
     print("Done")
